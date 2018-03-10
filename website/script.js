1341ebb4b054c5b590d2b57bc41773311a0fdc0a
snowyorb <mabrego@student.neumont.edu>
2018-03-09 20:50:45 -0700

    HABABABABBABABABABA

--- website/script.js
+++ website/script.js
@@ -2,16 +2,17 @@
 window.onload = function() {
 
     var currentTime = new Date().getHours();
-    if (currentTime == 1) {
+    if (currentTime == 20) {
         if (document.body) {
-            document.body.background = "http://img75.laughinggif.com/pic/HTTPS9tZWRpYS5naXBoeS5jb20vbWVkaWEvM282b3ptRjRaR09HM0RXWnlnL2dpcGh5LmdpZgloglog.gif";
+            document.h1.background = "http://img75.laughinggif.com/pic/HTTPS9tZWRpYS5naXBoeS5jb20vbWVkaWEvM282b3ptRjRaR09HM0RXWnlnL2dpcGh5LmdpZgloglog.gif";
         }
     }
     else {
         if (document.body) {
-            document.body.background = "https://s-media-cache-ak0.pinimg.com/originals/5f/c5/7b/5fc57b392c2c5c2423e527d60fa3e1d1.gif";
+            document.h1.background = "https://s-media-cache-ak0.pinimg.com/originals/5f/c5/7b/5fc57b392c2c5c2423e527d60fa3e1d1.gif";
         }
 
     }
+
 }
 
