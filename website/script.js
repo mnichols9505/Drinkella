bea765451e33909a2c60c432fe074902862d94bd
snowyorb <mabrego@student.neumont.edu>
2018-03-12 14:09:51 -0600

    HABABABABBABABABABA

--- website/script.js
+++ website/script.js
@@ -39,6 +39,22 @@ function growText2(){
     }
 }
 
+var slideIndex = 1;
+
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
+window.onload = function() { carousel(); };
+
 
 
 
